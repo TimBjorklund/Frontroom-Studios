9e0b49c1a07753dd17c451e5590b3401f454b27a
ElliotLBS <98454374+ElliotLBS@users.noreply.github.com>
2024-01-15 12:20:14 +0100

    noway

--- Assets/A-FrontRooms/Scripts/Jumpscare.cs
+++ Assets/A-FrontRooms/Scripts/Jumpscare.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class Jumpscare : MonoBehaviour
 {
-    public GameObject Player;
-    public GameObject JumpCam;
+   // public GameObject Player;
+   // public GameObject JumpCam;
+    Death death;
     // public AudioSource JumpscareSound;
     private void Start()
     {
-       JumpCam.SetActive(false);
+        death = GetComponent<Death>();
+     //  JumpCam.SetActive(false);
     }
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("MainCamera"))     //om triggern kolliderar med objectet med detectionTag så slutar fienden röra på sig
         {
-            print("Jumpscare");
+            death.die();
+            //print("Jumpscare");
             //JumpscareSound.Play();
-            JumpCam.SetActive(true);
-            Player.SetActive(false);
-
-          
-            StartCoroutine(Disableobject());
+            //JumpCam.SetActive(true);
+            //Player.SetActive(false);
+            //StartCoroutine(Disableobject());
         }
     }
-  IEnumerator Disableobject()
-    {
-        yield return new WaitForSeconds(2);
-        Player.SetActive(true);
-        JumpCam.SetActive(false);
-    }
+ // IEnumerator Disableobject()
+    //     yield return new WaitForSeconds(2);
+    //    Player.SetActive(true);
+       // JumpCam.SetActive(false);
+  //  }
 }
