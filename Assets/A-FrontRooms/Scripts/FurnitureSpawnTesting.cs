4e7430416fa66a0eef3cdef033a0a369f75740a0
TheoKjellberg <theo.kjellberg@elev.ga.lbs.se>
2024-01-16 21:53:03 +0100

    väggarna spawnar som flera tiles

--- Assets/A-FrontRooms/Scripts/FurnitureSpawnTesting.cs
+++ Assets/A-FrontRooms/Scripts/FurnitureSpawnTesting.cs
@@ -10,6 +10,9 @@ public class FurnitureSpawnTesting : AddedCommands
     public int roomSizeZ = 15;
     public int wallHeight = 3;
 
+    int wallLenghtX;
+    int wallLenghtZ;
+
     int wFurnitureX = -10;
     int wFurnitureZ = -10;
 
@@ -18,7 +21,6 @@ public class FurnitureSpawnTesting : AddedCommands
 
     bool wallFurnitureSpawned = false;
 
-    public Vector3 wallLocation;
     public GameObject wall;
 
     [SerializeField]
@@ -34,6 +36,9 @@ public class FurnitureSpawnTesting : AddedCommands
     {
         roomSizeX = Random.Range(5,30);
         roomSizeZ = Random.Range(5, 30);
+
+        wallLenghtX = roomSizeX;
+        wallLenghtZ = roomSizeZ;
 
         canSpawn = new int[roomSizeX, roomSizeZ];
         for (int x = 0; x < roomSizeX; x++)
@@ -59,30 +64,42 @@ public class FurnitureSpawnTesting : AddedCommands
         {
             if (i == 0)
             {
-                GameObject newObject = Instantiate(wall, currentLocation + new Vector3(roomSizeX / 2, wallHeight / 2, 0), Quaternion.identity) as GameObject;
-                newObject.transform.localScale = new Vector3(roomSizeX, wallHeight, 1.45f);
-                newObject.name = "wall 1";
+                for (int wallCount = 0; wallCount < wallLenghtX; wallCount++)
+                {
+                    GameObject newObject = Instantiate(wall, currentLocation + new Vector3(wallCount, wallHeight / 2, 0 /*gör tunnare väggar, t.ex -0.5, 0.95f*/), Quaternion.identity);
+                    newObject.transform.localScale = new Vector3(1, wallHeight, 1.45f);
+                    newObject.name = "wallX1 " + wallCount;
+                }
             }
             else if (i == 1)
             {
-                GameObject newObject = Instantiate(wall, currentLocation + new Vector3(0, wallHeight / 2, roomSizeZ / 2), Quaternion.identity) as GameObject;
-                newObject.transform.localScale = new Vector3(roomSizeZ, wallHeight, 1.45f);
-                newObject.transform.localRotation = Quaternion.Euler(0, 90, 0);
-                newObject.name = "wall 2";
+                for (int wallCount = 0; wallCount < wallLenghtZ; wallCount++)
+                {
+                    GameObject newObject = Instantiate(wall, currentLocation + new Vector3(0, wallHeight / 2, wallCount), Quaternion.identity);
+                    newObject.transform.localScale = new Vector3(1, wallHeight, 1.45f);
+                    newObject.transform.localRotation = Quaternion.Euler(0, 90, 0);
+                    newObject.name = "wallX2 " + wallCount;
+                }
             }
             else if (i == 2)
             {
-                GameObject newObject = Instantiate(wall, currentLocation + new Vector3(roomSizeX / 2, wallHeight / 2, roomSizeZ), Quaternion.identity) as GameObject;
-                newObject.transform.localScale = new Vector3(roomSizeX, wallHeight, 1.45f);
-                newObject.transform.localRotation = Quaternion.Euler(0, 180, 0);
-                newObject.name = "wall 3";
+                for (int wallCount = 0; wallCount < wallLenghtX; wallCount++)
+                {
+                    GameObject newObject = Instantiate(wall, currentLocation + new Vector3(wallCount, wallHeight / 2, wallLenghtZ), Quaternion.identity);
+                    newObject.transform.localScale = new Vector3(1, wallHeight, 1.45f);
+                    newObject.transform.localRotation = Quaternion.Euler(0, 180, 0);
+                    newObject.name = "wallX3 " + wallCount;
+                }
             }
             else if (i == 3)
             {
-                GameObject newObject = Instantiate(wall, currentLocation + new Vector3(roomSizeX, wallHeight / 2, roomSizeZ / 2), Quaternion.identity) as GameObject;
-                newObject.transform.localScale = new Vector3(roomSizeZ, wallHeight, 1.45f);
-                newObject.transform.localRotation = Quaternion.Euler(0, 270, 0);
-                newObject.name = "wall 4";
+                for (int wallCount = 0; wallCount < wallLenghtZ; wallCount++)
+                {
+                    GameObject newObject = Instantiate(wall, currentLocation + new Vector3(wallLenghtX, wallHeight / 2, wallCount), Quaternion.identity);
+                    newObject.transform.localScale = new Vector3(1, wallHeight, 1.45f);
+                    newObject.transform.localRotation = Quaternion.Euler(0, 270, 0);
+                    newObject.name = "wallX4 " + wallCount;
+                }
             }
 
         }
