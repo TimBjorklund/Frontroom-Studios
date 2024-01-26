ff050e6468ff884f033441a1abbc34c200ce46f2
ElliotLBS <98454374+ElliotLBS@users.noreply.github.com>
2024-01-26 12:18:07 +0100

    😎😊😊🤩

--- Assets/A-FrontRooms/Scripts/KeyHoleScript.cs
+++ Assets/A-FrontRooms/Scripts/KeyHoleScript.cs
@@ -17,12 +17,6 @@ public class KeyHoleScript : MonoBehaviour
     [SerializeField]
     GameObject Lock;
 
-    [SerializeField]
-    float xR;
-    [SerializeField]
-    float yR;
-    [SerializeField]
-    float zR;
 
 
     [SerializeField]
@@ -30,24 +24,33 @@ public class KeyHoleScript : MonoBehaviour
 
     public bool Unlocked = false;
 
+    public bool rightKey;
+
     void Update()
     {
+
+        //Om specfik nyckelnamn socketerar med specfik låsnamn då, gör bla bla bla
         if (Lock == null)
         {
             Unlocked = true;
         }
+ 
+      
         if (socket.hasHover)
         {
-            Unlocked = true;
-            if (Lock.transform.position.y > floorPoint)
             {
-                Vector3 temp = new Vector3(0, 3f * Time.deltaTime, 0);
-                Lock.transform.position -= temp;
+                Unlocked = true;
+                if (Lock.transform.position.y > floorPoint)
+                {
+                    Vector3 temp = new Vector3(0, 3f * Time.deltaTime, 0);
+                    Lock.transform.position -= temp;
+                }
+                if (Lock.transform.rotation.eulerAngles.z < 90) //90
+                {
+                    Lock.transform.Rotate(0f, 0f, 180.0f * Time.deltaTime); //180.0f
+                }
             }
-            if (Lock.transform.rotation.eulerAngles.z < 90) //90
-            {
-                Lock.transform.Rotate(xR, yR, 180.0f * Time.deltaTime); //180.0f
-            }
+          
 
 
             //OpenDoor();
